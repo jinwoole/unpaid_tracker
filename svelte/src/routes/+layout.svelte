c99f63f78dcccdda84abe19f3e4ceeb0293cd8d8
lee <you@example.com>
2024-10-18 07:06:31 +0000

    v 0.1.0

---
+++ svelte/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.postcss';
+	import { autoModeWatcher } from '@skeletonlabs/skeleton';
+
+</script>
+
+<!-- 그 뭐냐 다크모드 그거 -->
+<svelte:head>{@html '<script>(' + autoModeWatcher.toString() + ')();</script>'}</svelte:head>
+
+<slot />
