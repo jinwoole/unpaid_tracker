30b3f8101aed0326183b1704f023068ce996a866
lee <you@example.com>
2024-10-23 05:59:05 +0000

    0.6.3 dark mode

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 	import '../app.postcss';
+	import { onMount } from 'svelte';
 	import { autoModeWatcher } from '@skeletonlabs/skeleton';
 
+	onMount(() => {										
+		autoModeWatcher();
+	})
 </script>
 
 <!-- 그 뭐냐 다크모드 그거 -->
-<svelte:head>{@html '<script>(' + autoModeWatcher.toString() + ')();</script>'}</svelte:head>
-
 <slot />
